f7cf61d79c1483e3683e9fa2fb3b47a42120794f
Dion Ang <dion@Dions-MacBook-Pro.local>
2023-02-10 23:45:51 +0800

    First set up
    Set up react app, created nav bar component and got started with styling.
    
    Imported google fonts for the logo

---
+++ frontend-react/src/NavBar/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import styles from "../styles/navbar.module.css";
+
+const NavBar = () => {
+  return (
+    <nav className={styles.navBar}>
+      <logo className={styles.logo}>RemoteSG</logo>
+      <div className={styles.navLinks}>
+        <div className={styles.getJobAlert}>Get job alerts</div>
+        <div className={styles.postAJob}>Post a job</div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
