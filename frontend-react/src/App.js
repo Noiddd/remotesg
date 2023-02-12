55f1c7841e99765306fee93619008a73677bbdf1
Dion Ang <dion@Dions-MacBook-Pro.local>
2023-02-12 23:36:11 +0800

    Filter design clean up
    Change up the design a whole lot.
    
    Removed filter buttons. now its just a search bar and filter by salary and job category

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,16 +1,17 @@
-import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./NavBar/NavBar";
 import styles from "./styles/Home/home.module.css";
-import JobFilter from "./Filter/JobFilter";
+import SearchBar from "./SearchBar/SearchBar";
+import FilterSalary from "./FilterJob/FilterSalary";
 
 function App() {
   return (
     <>
       <NavBar />
-      <div className={styles.mainContainer}>
-        <JobFilter />
-      </div>
+      <div className={styles.mainContainer}></div>
+      <SearchBar />
+      <FilterSalary />
+      <div>Over 1,000 remote jobs in Singapore</div>
     </>
   );
 }
---
+++ frontend-react/src/FilterJob/FilterSalary.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FilterSalary = () => {
+  return <div>FilterSalary</div>;
+};
+
+export default FilterSalary;
